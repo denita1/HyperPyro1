eb1de8e7dbb79e3398245c07d216ea966ed87d69
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-06 16:29:09 +0700

    Update create.py

--- HyperPyro/modules/create.py
+++ HyperPyro/modules/create.py
@@ -5,13 +5,13 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/SharingUserbot & t.me/HyperSupportQ 
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from config import CMD_HANDLER as cmd
-from rams.helpers.basic import edit_or_reply
+from HyperPyro.helpers.basic import edit_or_reply
 
 from .help import add_command_help
 
