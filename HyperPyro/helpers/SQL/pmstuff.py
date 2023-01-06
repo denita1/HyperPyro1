d05cf209022ccb94042baa9429f9330777a77a3a
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-06 17:04:49 +0700

    Update pmstuff.py

--- HyperPyro/helpers/SQL/pmstuff.py
+++ HyperPyro/helpers/SQL/pmstuff.py
@@ -1,5 +1,5 @@
 try:
-    from rams.helpers.SQL import BASE, SESSION
+    from HyperPyro.helpers.SQL import BASE, SESSION
 except ImportError:
     raise AttributeError
 from sqlalchemy import Column
