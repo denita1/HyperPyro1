0dfd4799204f7bae73c2032aca163aa8cb28f582
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-06 14:59:12 +0700

    Update stats.py

--- HyperPyro/modules/stats.py
+++ HyperPyro/modules/stats.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/SharingUserbot & t.me/HyperSupportQ 
 
 from datetime import datetime
 
@@ -13,7 +13,7 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
 from config import CMD_HANDLER as cmd
-from rams.helpers.basic import edit_or_reply
+from HyperPyro.helpers.basic import edit_or_reply
 
 from .help import *
 
