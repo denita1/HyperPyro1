8f168fc91d5240ccbc2210c6029f03281951c488
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-06 18:24:22 +0700

    Update __main__.py

--- HyperPyro/__main__.py
+++ HyperPyro/__main__.py
@@ -34,7 +34,7 @@ async def main():
                 f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
             )
         except Exception as a:
-            LOGGER("dragons").warning(a)
+            LOGGER("main").warning(a)
     LOGGER("HyperPyro").info(f"HyperPyro-Bot v{BOT_VER} [🔥 UDAH AKTIF BLOK! 🔥]")
     if not str(BOTLOG_CHATID).startswith("-100"):
         await create_botlog(bot1)
