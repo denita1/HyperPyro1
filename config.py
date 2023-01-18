b0c89291233ad92eadfa066f99544057918c2001
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-18 13:46:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001883961446]
+    BLACKLIST_CHAT = [-1001883961446, -1001883961446]
 BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "0.3.1@master"
