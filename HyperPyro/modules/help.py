6924753fe2665c6a3852bc9bc61c1ac573f63d66
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-20 19:34:31 +0700

    Update help.py

--- HyperPyro/modules/help.py
+++ HyperPyro/modules/help.py
@@ -7,37 +7,59 @@
 #
 # t.me/SharingUserbot & t.me/HyperSupportQ 
 
+import asyncio
 from prettytable import PrettyTable
 from pyrogram import Client, enums, filters
 from pyrogram.types import Message
+from geezlibs.ram.helpers.basic import edit_or_reply
+from geezlibs.ram.helpers.utility import split_list
+from config import CMD_HANDLER as cmd
+from rams import CMD_HELP,app
 
-from config import CMD_HANDLER
-from HyperPyro import CMD_HELP
-from HyperPyro.helpers.basic import edit_or_reply
-from HyperPyro.helpers.utility import split_list
+def ReplyCheck(message: Message):
+    reply_id = None
 
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.id
 
-@Client.on_message(filters.command("help", CMD_HANDLER) & filters.me)
+    elif not message.from_user.is_self:
+        reply_id = message.id
+
+    return reply_id
+
+@Client.on_message(filters.command(["help", "helpme"], cmd) & filters.me)
 async def module_help(client: Client, message: Message):
     cmd = message.command
     help_arg = ""
+    bot_username = (await app.get_me()).username
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
-    elif message.reply_to_message and len(cmd) == 1:
-        help_arg = message.reply_to_message.text
     elif not message.reply_to_message and len(cmd) == 1:
-        ac = PrettyTable()
-        ac.header = False
-        ac.title = "Hyper-UserBot Modules"
-        ac.align = "l"
-        for x in split_list(sorted(CMD_HELP.keys()), 2):
-            ac.add_row([x[0], x[1] if len(x) >= 2 else None])
-        await edit_or_reply(
-            message, f"```{str(ac)}```\n• @storyQi × @HyperSupportQ •"
-        )
-        await message.reply(
-            f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
-        )
+        try:
+            nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
+            await asyncio.gather(
+                message.delete(),
+                client.send_inline_bot_result(
+                    message.chat.id, nice.query_id, nice.results[0].id
+                ),
+            )
+        except BaseException as e:
+            print(f"{e}")
+            ac = PrettyTable()
+            ac.header = False
+            ac.title = "HyperRobot Plugins"
+            ac.align = "l"
+            for x in split_list(sorted(CMD_HELP.keys()), 2):
+                ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+            xx = await client.send_message(
+                message.chat.id,
+                f"```{str(ac)}```\n• @storyQi >< @HyperSupportQ •",
+                reply_to_message_id=ReplyCheck(message),
+            )
+            await xx.reply(
+                f"**Usage:** `{cmd}help broadcast` **To View Module Information**"
+            )
+            return
 
     if help_arg:
         if help_arg in CMD_HELP:
@@ -45,7 +67,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @storyQi"
+            this_command += "© @HyperSupportQ"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -55,6 +77,43 @@ async def module_help(client: Client, message: Message):
                 f"`{help_arg}` **Bukan Nama Modul yang Valid.**",
             )
 
+@Client.on_message(filters.command(["helper", "rhelp"], cmd) & filters.me)
+async def module_helper(client: Client, message: Message):
+    cmd = message.command
+    help_arg = ""
+    if len(cmd) > 1:
+        help_arg = " ".join(cmd[1:])
+    elif message.reply_to_message and len(cmd) == 1:
+        help_arg = message.reply_to_message.text
+    elif not message.reply_to_message and len(cmd) == 1:
+        ac = PrettyTable()
+        ac.header = False
+        ac.title = "HyperRobot Plugins"
+        ac.align = "l"
+        for x in split_list(sorted(CMD_HELP.keys()), 2):
+            ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+        await edit_or_reply(
+            message, f"```{str(ac)}```\n• @storyQi >< @HyperSupportQ •"
+        )
+        await message.reply(
+            f"**Usage**:`{cmd}help broadcast` **To View Module details**"
+        )
+
+    if help_arg:
+        if help_arg in CMD_HELP:
+            commands: dict = CMD_HELP[help_arg]
+            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            for x in commands:
+                this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+            this_command += "© @storyQi >< @HyperSupportQ"
+            await edit_or_reply(
+                message, this_command, parse_mode=enums.ParseMode.MARKDOWN
+            )
+        else:
+            await edit_or_reply(
+                message,
+                f"`{help_arg}` **Not a Valid Module Name.**",
+            )
 
 def add_command_help(module_name, commands):
     if module_name in CMD_HELP.keys():
@@ -68,3 +127,4 @@ def add_command_help(module_name, commands):
                 command_dict[x[0]] = x[1]
 
     CMD_HELP[module_name] = command_dict
+#null
