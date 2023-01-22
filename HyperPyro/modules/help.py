cf06211e58eebafd0280b5e7f1782fc4bbf1d0bd
Ling-ex <117170541+Ling-ex@users.noreply.github.com>
2023-01-22 13:13:13 +0700

    Update help.py

--- HyperPyro/modules/help.py
+++ HyperPyro/modules/help.py
@@ -12,9 +12,9 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
 from config import CMD_HANDLER
-from rams import CMD_HELP
-from rams.helpers.basic import edit_or_reply
-from rams.helpers.utility import split_list
+from HyperPyro import CMD_HELP
+from HyperPyro.helpers.basic import edit_or_reply
+from HyperPyro.helpers.utility import split_list
 
 
 @Client.on_message(filters.command("help", CMD_HANDLER) & filters.me)
@@ -33,7 +33,7 @@ async def module_help(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @UserbotCh × @Ramsupportt •"
+            message, f"```{str(ac)}```\n• @storyQi × @HyperSupportQ •"
         )
         await message.reply(
             f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
@@ -45,7 +45,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @UserbotCh"
+            this_command += "© @storyQi"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
